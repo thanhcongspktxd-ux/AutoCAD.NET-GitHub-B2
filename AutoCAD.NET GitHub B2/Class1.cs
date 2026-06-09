5a112b7ceac86f4820b1afbe1f84417e006e5191
Công Nguyễn <thanhcongspktxd@gmail.com>
2026-06-09 21:45:26 +0700

    Add comment for command BT1

--- AutoCAD.NET GitHub B2/Class1.cs
+++ AutoCAD.NET GitHub B2/Class1.cs
@@ -15,24 +15,24 @@ namespace AutoCAD.NET_GitHub_B2
         [CommandMethod("TEXTUPPER")]
         public void Upper()
         {
+            // Khai báo Editor giao tiếp với AutoCAD
             Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
-
+            //Tạo Option Nhập chuỗi
             PromptStringOptions pso = new PromptStringOptions("\nEnter your name");
-
+            //Kiểm tra và cho phép có Space
             pso.AllowSpaces = true;
-
+            //Nhận dữ liệu từ USER
             PromptResult result = ed.GetString(pso);
-
+            //Kiểm tra user có hủy lệnh không
             if (result.Status != PromptStatus.OK)
             {
-                ed.WriteMessage("\nCommand Cancel")
+                ed.WriteMessage("\nCommand Cancel");
                     return;
             }
-
+            //Dùng string khai báo biến và gán biến
             String name = result.StringResult;
-
+            //Xuất kết quả kèm theo chuyển thành chữ in hoa
             ed.WriteMessage($"\nHELLO {name.ToUpper()}");
-
         }
     }
 }
