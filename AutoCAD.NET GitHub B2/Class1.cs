99d56b3c7612bfe9768c043a82bd39955189484c
Công Nguyễn <thanhcongspktxd@gmail.com>
2026-06-09 21:38:38 +0700

    Add Command TEXTUPPER

--- AutoCAD.NET GitHub B2/Class1.cs
+++ AutoCAD.NET GitHub B2/Class1.cs
@@ -12,7 +12,31 @@ namespace AutoCAD.NET_GitHub_B2
 {
     public class B2
     {
+        [CommandMethod("TEXTUPPER")]
+        public void Upper()
+        {
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
 
+            PromptStringOptions pso = new PromptStringOptions("\nEnter your name");
 
+            pso.AllowSpaces = true;
+
+            PromptResult result = ed.GetString(pso);
+
+            if (result.Status != PromptStatus.OK)
+            {
+                ed.WriteMessage("\nCommand Cancel")
+                    return;
+            }
+
+            String name = result.StringResult;
+
+            ed.WriteMessage($"\nHELLO {name.ToUpper()}");
+
+        }
     }
 }
+  
+
+
+
