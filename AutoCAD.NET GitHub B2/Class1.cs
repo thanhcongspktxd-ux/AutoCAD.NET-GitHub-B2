063c737683dc3eac595f8177d9adb8b6f98d8b86
Công Nguyễn <thanhcongspktxd@gmail.com>
2026-06-10 21:25:18 +0700

    Add command Circle Area

--- AutoCAD.NET GitHub B2/Class1.cs
+++ AutoCAD.NET GitHub B2/Class1.cs
@@ -12,7 +12,30 @@ namespace AutoCAD.NET_GitHub_B2
 {
     public class B2
     {
+        [CommandMethod("CIRCLEAREA")]
+        public void CircleArea()
+        {
+            // Khai báo Editor giao tiếp với AutoCAD
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            //Tạo Option nhập bán kính
+            PromptDoubleOptions r = new PromptDoubleOptions("\nNhập bán kính");
+            //Kiểm tra và cho phép 
+            r.AllowNegative = false;
+            r.AllowZero = false;
+            //Nhận dữ liệu từ USER
+            PromptDoubleResult rs = ed.GetDouble(r);
+            //Kiểm tra user có hủy lệnh không
+            if (rs.Status != PromptStatus.OK)
+            {
+                ed.WriteMessage("\nCommand Cancel");
+                return;
+            }
+            //Gán double cho Area 
+            double area = Math.PI * Math.Pow(rs.Value, 2);
 
-
+            //Xuất kết quả diện tích của hình tròn
+            ed.WriteMessage($"\nArea Circle: {area:F2}");
+        }
     }
 }
+
