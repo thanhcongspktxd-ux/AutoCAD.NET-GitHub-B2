8909555d61771e276c3c6fe447b3954a981cd006
Công Nguyễn <thanhcongspktxd@gmail.com>
2026-06-12 20:34:37 +0700

    Add command NameAge

--- AutoCAD.NET GitHub B2/Class1.cs
+++ AutoCAD.NET GitHub B2/Class1.cs
@@ -12,7 +12,43 @@ namespace AutoCAD.NET_GitHub_B2
 {
     public class B2
     {
+        [CommandMethod("NAMEAGE")]
+        public void NameAge()
+        {
+            // Khai báo Editor giao tiếp với AutoCAD
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
 
+            // Chọn điểm thứ nhất
+            PromptStringOptions pso =
+                new PromptStringOptions("\nNhập Tên:");
 
+            PromptResult prs = ed.GetString(pso);
+
+            // Kiểm tra User có hủy lệnh không
+            if (prs.Status != PromptStatus.OK)
+            {
+                ed.WriteMessage("\nCommand Cancel");
+                return;
+            }
+
+            String name = prs.StringResult;
+
+            // Nhập tuổi
+            PromptDoubleOptions pdo = new PromptDoubleOptions("\nNhập Tuổi:");
+            pdo.AllowNegative = false;
+            pdo.AllowZero = false;
+            PromptDoubleResult pdr = ed.GetDouble(pdo);
+
+            // Kiểm tra User có hủy lệnh không
+            if (pdr.Status != PromptStatus.OK)
+            {
+                ed.WriteMessage("\nCommand Cancel");
+                return;
+            }
+
+            Double age = pdr.Value;
+
+            ed.WriteMessage($"\nTên và Tuổi của bạn là: {name} và {age} ");
+        }
     }
 }
