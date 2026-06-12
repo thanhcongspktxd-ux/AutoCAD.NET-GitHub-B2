8be7d65286c932ed240891bb25d34b0b62214ebf
Công Nguyễn <thanhcongspktxd@gmail.com>
2026-06-12 19:38:40 +0700

    Add command Distance

--- AutoCAD.NET GitHub B2/Class1.cs
+++ AutoCAD.NET GitHub B2/Class1.cs
@@ -12,7 +12,34 @@ namespace AutoCAD.NET_GitHub_B2
 {
     public class B2
     {
+        [CommandMethod("DISTANCE2PT")]
+        public void DisTance2Pt()
+        {
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            // Chọn điểm đầu tiên
+            PromptPointOptions pt = new PromptPointOptions("\nChọn điểm đầu tiên ");
+            pt.AllowNone = true;
+            PromptPointResult pt1 = ed.GetPoint(pt);
 
+            if (pt1.Status != PromptStatus.OK)
+            {
+                ed.WriteMessage("\nCommand Cancel");
+                return;
+            }
+            // Chọn điểm đầu thứ 2
+            PromptPointOptions ptt = new PromptPointOptions("\nChọn điểm thứ hai ");
+            ptt.AllowNone = true;
+            PromptPointResult pt2 = ed.GetPoint(ptt);
 
+            if (pt2.Status != PromptStatus.OK)
+            {
+                ed.WriteMessage("\nCommand Cancel");
+                return;
+            }
+
+            //Tính khoáng cách 
+            double distance = pt1.Value.DistanceTo(pt2.Value);
+            ed.WriteMessage($"\nDistance: {distance}");
+        }
     }
 }
